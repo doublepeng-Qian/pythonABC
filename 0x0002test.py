1feaee3965881157fbf4e67008ca2862e2c20c06
unknown <qianpengpeng@nse.cn.com>
2020-03-24 23:41:53 +0800

    First add.

---
+++ 0x0002test.py
@@ -0,0 +1,39 @@
+
+# test_1
+print("test_1:")
+the_world_is_flat = True
+if the_world_is_flat:
+    print("Be careful not to fall off!")
+
+
+# test_2
+print("test_2:")
+# this is the first comment
+spam = 1 # and this is the second comment
+         # ... and not a third!
+text = "# This is not a comment because it's inside quotes."
+if spam == 1:
+    print(text)
+
+
+# test_3
+print("test_3:")
+sum = 5+5
+print(sum)
+
+result1 = 8 / 5
+print(result1)
+
+result2 = 17 / 3
+print(result2)
+
+
+# test_4
+word = 'Pythonn'
+print("test_4:")
+print(word)
+print(word[5])
+print(word[-1])
+
+# test_5
+print("test_5:")
